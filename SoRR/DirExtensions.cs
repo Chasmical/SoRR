6115890ed4f4df56fe1c795d906cd26bb13144ed
Chasmical <hillankeyxl2@yandex.ru>
2024-09-12 16:50:09 +0700

    ✨ Add direction enums, utilities and extensions

---
+++ SoRR/DirExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace SoRR
+{
+    public static class DirExtensions
+    {
+        [Pure] public static int ToDegrees(this Dir4 dir)
+            => ToDegreesCore(dir);
+        [Pure] public static int ToDegrees(this Dir8 dir)
+            => ToDegreesCore(dir);
+        [Pure] public static int ToDegrees(this Dir24 dir)
+            => ToDegreesCore(dir);
+
+        [Pure] private static int ToDegreesCore<TDir>(TDir dir)
+            => Unsafe.As<TDir, int>(ref dir) * (360 / Direction.CountOf<TDir>());
+
+        [Pure] public static float ToRadians(this Dir4 dir)
+            => ToRadiansCore(dir);
+        [Pure] public static float ToRadians(this Dir8 dir)
+            => ToRadiansCore(dir);
+        [Pure] public static float ToRadians(this Dir24 dir)
+            => ToRadiansCore(dir);
+
+        [Pure] private static float ToRadiansCore<TDir>(TDir dir)
+            => Unsafe.As<TDir, int>(ref dir) * (2f * Mathf.PI / Direction.CountOf<TDir>());
+
+        private static readonly string[] directions8 = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+
+        [Pure] public static string ToLetters(this Dir4 dir)
+            => directions8[(int)dir * 2];
+        [Pure] public static string ToLetters(this Dir8 dir)
+            => directions8[(int)dir];
+
+        [Pure] public static Dir4 Rotate(this Dir4 dir, int delta)
+            => Direction.Normalize(RotateCore(dir, delta));
+        [Pure] public static Dir8 Rotate(this Dir8 dir, int delta)
+            => Direction.Normalize(RotateCore(dir, delta));
+        [Pure] public static Dir24 Rotate(this Dir24 dir, int delta)
+            => Direction.Normalize(RotateCore(dir, delta));
+
+        [Pure] public static Dir4 Clockwise(this Dir4 dir)
+            => Direction.Normalize(RotateCore(dir, 1));
+        [Pure] public static Dir8 Clockwise(this Dir8 dir)
+            => Direction.Normalize(RotateCore(dir, 1));
+        [Pure] public static Dir24 Clockwise(this Dir24 dir)
+            => Direction.NormalizeFast(RotateCore(dir, 1));
+
+        [Pure] private static TDir RotateCore<TDir>(TDir dir, int delta)
+        {
+            int result = Unsafe.As<TDir, int>(ref dir) + delta;
+            return Unsafe.As<int, TDir>(ref result);
+        }
+
+        // the conversion methods can't be generalized properly without losing performance
+
+        [Pure] public static Dir8 ToDir8(this Dir4 dir)
+            => (Dir8)((int)dir * 2);
+        [Pure] public static Dir24 ToDir24(this Dir4 dir)
+            => (Dir24)((int)dir * 6);
+
+        [Pure] public static Dir4 ToDir4(this Dir8 dir)
+            => (Dir4)((int)dir / 2);
+        [Pure] public static Dir24 ToDir24(this Dir8 dir)
+            => (Dir24)((int)dir * 3);
+
+        [Pure] public static Dir4 ToDir4(this Dir24 dir)
+            => (Dir4)((((int)dir + 2) / 6) & 3);
+        [Pure] public static Dir8 ToDir8(this Dir24 dir)
+            => (Dir8)((((int)dir + 1) / 3) & 7);
+
+    }
+}
---
+++ SoRR/Directions.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace SoRR
+{
+    public static class Direction
+    {
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int CountOf<T>()
+        {
+            if (typeof(T) == typeof(Dir4)) return 4;
+            if (typeof(T) == typeof(Dir8)) return 8;
+            if (typeof(T) == typeof(Dir24)) return 24;
+            throw new ArgumentException($"Type {typeof(T)} is not a direction enum.", nameof(T));
+        }
+
+        [Pure] public static Dir4 DegreesToDir4(float degrees)
+            => Normalize(UnitsToDirCore<Dir4>(degrees, 360f));
+        [Pure] public static Dir8 DegreesToDir8(float degrees)
+            => Normalize(UnitsToDirCore<Dir8>(degrees, 360f));
+        [Pure] public static Dir24 DegreesToDir24(float degrees)
+            => NormalizeFast(UnitsToDirCore<Dir24>(degrees, 360f));
+
+        [Pure] public static Dir4 RadiansToDir4(float radians)
+            => Normalize(UnitsToDirCore<Dir4>(radians, 2f * Mathf.PI));
+        [Pure] public static Dir8 RadiansToDir8(float radians)
+            => Normalize(UnitsToDirCore<Dir8>(radians, 2f * Mathf.PI));
+        [Pure] public static Dir24 RadiansToDir24(float radians)
+            => NormalizeFast(UnitsToDirCore<Dir24>(radians, 2f * Mathf.PI));
+
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static TDir UnitsToDirCore<TDir>(float units, float fullRotation)
+        {
+            int value = Mathf.RoundToInt(units * (CountOf<TDir>() / fullRotation));
+            return Unsafe.As<int, TDir>(ref value);
+        }
+
+        [Pure] public static bool IsValid(Dir4 dir)
+            => IsValidCore(dir);
+        [Pure] public static bool IsValid(Dir8 dir)
+            => IsValidCore(dir);
+        [Pure] public static bool IsValid(Dir24 dir)
+            => IsValidCore(dir);
+
+        [Pure] private static bool IsValidCore<TDir>(TDir dir)
+            => Unsafe.As<TDir, uint>(ref dir) < CountOf<TDir>();
+
+        [Pure] public static Dir4 Normalize(Dir4 dir)
+            => (Dir4)((int)dir & 3);
+        [Pure] public static Dir8 Normalize(Dir8 dir)
+            => (Dir8)((int)dir & 7);
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Dir24 Normalize(Dir24 dir)
+        {
+            int result = (int)dir % 24;
+            return (Dir24)(result < 0 ? result + 24 : result);
+        }
+        [Pure] public static Dir24 NormalizeFast(Dir24 dir)
+            => dir == (Dir24)24 ? 0 : dir;
+
+    }
+    public enum Dir4
+    {
+        North,
+        West,
+        South,
+        East,
+    }
+    public enum Dir8
+    {
+        North,
+        NorthWest,
+        West,
+        SouthWest,
+        South,
+        SouthEast,
+        East,
+        NorthEast,
+    }
+    public enum Dir24
+    {
+        // ReSharper disable InconsistentNaming
+        N,
+        NNNE,
+        NNE,
+        NE,
+        NEE,
+        NEEE,
+        E,
+        SEEE,
+        SEE,
+        SE,
+        SSE,
+        SSSE,
+        S,
+        SSSW,
+        SSW,
+        SW,
+        SWW,
+        SWWW,
+        W,
+        NWWW,
+        NWW,
+        NW,
+        NNW,
+        NNNW,
+        // ReSharper restore InconsistentNaming
+    }
+}

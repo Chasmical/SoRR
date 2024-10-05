186a7c317bdc35800ef236384799abefc05b6ff0
Chasmical <hillankeyxl2@yandex.ru>
2024-10-05 16:25:38 +0700

    🩹 Use System's Debug.Assert instead of Unity's

---
+++ SoRR/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿
+// UnityEngine.Debug.Assert doesn't work as you would expect it to.
+// Instead of throwing on assertion error, it only logs it instead.
+global using Debug = System.Diagnostics.Debug;

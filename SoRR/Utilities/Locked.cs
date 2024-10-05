dd7113dab82dcaa8c397599fe3e1a1ae116c6a92
Chasmical <hillankeyxl2@yandex.ru>
2024-10-05 16:26:14 +0700

    ✨ Add Locked static class, add Utilities/ folder

---
+++ SoRR/Utilities/Locked.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace SoRR
+{
+    public static class Locked
+    {
+        public static T Get<T>(ref T? field, object lockObj, [InstantHandle] Func<T> initializeFunc) where T : class
+        {
+            T? current = field;
+            if (current is null)
+            {
+                lock (lockObj)
+                {
+                    current = field;
+                    if (current is null) field = current = initializeFunc();
+                }
+            }
+            return current;
+        }
+
+        public static T GetConcurrent<T>(ref T? field, object lockObj, [InstantHandle] Func<T> initializeFunc) where T : class
+        {
+            T? current = field;
+            if (current is null)
+            {
+                current = initializeFunc();
+                lock (lockObj) current = field ??= current;
+            }
+            return current;
+        }
+
+    }
+}

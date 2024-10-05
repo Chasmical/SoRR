4f0f2a21492bc63bbbfb6d87cef135dde1aff911
Chasmical <hillankeyxl2@yandex.ru>
2024-10-05 21:05:47 +0700

    ✨ Add MainThread for re-entering the main thread

---
+++ SoRR/Components/MainThread.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using Chasm.Utilities;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace SoRR
+{
+    public static class MainThread
+    {
+        private static readonly ConcurrentQueue<Action> queue = [];
+        private static MainThreadRunner? activeRunner;
+
+        public static void Enqueue(Action action)
+        {
+            // TODO: log a warning, if activeRunner is null
+            queue.Enqueue(action);
+        }
+
+        [UsedImplicitly]
+        public sealed class MainThreadRunner : MonoBehaviour
+        {
+            private readonly List<Action> actionsList = [];
+            private readonly List<Exception> exceptionsList = [];
+
+            public void FixedUpdate()
+            {
+                List<Action> actions = actionsList;
+                List<Exception> exceptions = exceptionsList;
+
+                while (queue.TryDequeue(out Action? action))
+                    actions.Add(action);
+
+                for (int i = 0; i < actions.Count; i++)
+                {
+                    if (Util.Catch(actions[i]) is { } exception)
+                        exceptions.Add(exception);
+                }
+                actions.Clear();
+
+                if (exceptions.Count > 0)
+                {
+                    Exception exception = exceptions.Count == 1 ? exceptions[0] : new AggregateException(exceptions);
+                    exceptions.Clear();
+                    throw exception;
+                }
+            }
+
+            // There can only be one
+            public void OnEnable()
+                => enabled = Interlocked.CompareExchange(ref activeRunner, this, null) is null;
+            public void OnDisable()
+                => Interlocked.CompareExchange(ref activeRunner, null, this);
+
+        }
+    }
+}
--- SoRR/Entrypoint.cs
+++ SoRR/Entrypoint.cs
@@ -5,8 +5,14 @@ namespace SoRR
 {
     public static class Entrypoint
     {
+        public static GameObject GlobalsObject { get; private set; } = null!;
+
         public static void Run()
         {
+            GlobalsObject = new GameObject("Globals") { isStatic = true };
+
+            GlobalsObject.GetOrAddComponent<MainThread.MainThreadRunner>();
+
         }
 
     }

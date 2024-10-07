10cee4101e6232a6d7328ffb1f6d1fb892a8527f
Chasmical <hillankeyxl2@yandex.ru>
2024-10-07 14:56:53 +0700

    📝 Add XML docs for direction enums and types

--- SoRR/DirExtensions.cs
+++ SoRR/DirExtensions.cs
@@ -7,20 +7,50 @@ namespace SoRR
 {
     public static class DirExtensions
     {
+        /// <summary>
+        ///   <para>Converts the specified <paramref name="dir"/> to a rotation angle, in degrees clockwise.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to convert to degrees.</param>
+        /// <returns>A rotation angle, in degrees clockwise, between 0 and 360° (exclusive), corresponding to the specified <paramref name="dir"/>.</returns>
         [Pure] public static int ToDegrees(this Dir4 dir)
             => ToDegreesCore(dir);
+        /// <summary>
+        ///   <para>Converts the specified <paramref name="dir"/> to a rotation angle, in degrees clockwise.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to convert to degrees.</param>
+        /// <returns>A rotation angle, in degrees clockwise, between 0 and 360° (exclusive), corresponding to the specified <paramref name="dir"/>.</returns>
         [Pure] public static int ToDegrees(this Dir8 dir)
             => ToDegreesCore(dir);
+        /// <summary>
+        ///   <para>Converts the specified <paramref name="dir"/> to a rotation angle, in degrees clockwise.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to convert to degrees.</param>
+        /// <returns>A rotation angle, in degrees clockwise, between 0 and 360° (exclusive), corresponding to the specified <paramref name="dir"/>.</returns>
         [Pure] public static int ToDegrees(this Dir24 dir)
             => ToDegreesCore(dir);
 
         [Pure] private static int ToDegreesCore<TDir>(TDir dir)
             => Unsafe.As<TDir, int>(ref dir) * (360 / Direction.CountOf<TDir>());
 
+        /// <summary>
+        ///   <para>Converts the specified <paramref name="dir"/> to a rotation angle, in radians clockwise.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to convert to radians.</param>
+        /// <returns>A rotation angle, in radians clockwise, between 0 and 2π (exclusive), corresponding to the specified <paramref name="dir"/>.</returns>
         [Pure] public static float ToRadians(this Dir4 dir)
             => ToRadiansCore(dir);
+        /// <summary>
+        ///   <para>Converts the specified <paramref name="dir"/> to a rotation angle, in radians clockwise.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to convert to radians.</param>
+        /// <returns>A rotation angle, in radians clockwise, between 0 and 2π (exclusive), corresponding to the specified <paramref name="dir"/>.</returns>
         [Pure] public static float ToRadians(this Dir8 dir)
             => ToRadiansCore(dir);
+        /// <summary>
+        ///   <para>Converts the specified <paramref name="dir"/> to a rotation angle, in radians clockwise.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to convert to radians.</param>
+        /// <returns>A rotation angle, in radians clockwise, between 0 and 2π (exclusive), corresponding to the specified <paramref name="dir"/>.</returns>
         [Pure] public static float ToRadians(this Dir24 dir)
             => ToRadiansCore(dir);
 
@@ -29,26 +59,77 @@ namespace SoRR
 
         private static readonly string[] directions24 = Enum.GetNames(typeof(Dir24));
 
+        /// <summary>
+        ///   <para>The length of the longest letters-string of a direction enum: <see cref="Dir24.NNNE"/>.</para>
+        /// </summary>
         public const int MaxToLettersLength = 4;
 
+        /// <summary>
+        ///   <para>Returns the initial corresponding to the specified cardinal <paramref name="dir"/>.</para>
+        /// </summary>
+        /// <param name="dir">The cardinal direction to get the initial of.</param>
+        /// <returns>The initial corresponding to the specified <paramref name="dir"/>.</returns>
         [Pure] public static string ToLetters(this Dir4 dir)
             => dir.ToDir24().ToLetters();
+        /// <summary>
+        ///   <para>Returns the initial corresponding to the specified cardinal or ordinal <paramref name="dir"/>.</para>
+        /// </summary>
+        /// <param name="dir">The cardinal or ordinal direction to get the initial of.</param>
+        /// <returns>The initial corresponding to the specified <paramref name="dir"/>.</returns>
         [Pure] public static string ToLetters(this Dir8 dir)
             => dir.ToDir24().ToLetters();
+        /// <summary>
+        ///   <para>Returns the initial corresponding to the specified <paramref name="dir"/>.</para>
+        /// </summary>
+        /// <param name="dir">The direction to get the initial of.</param>
+        /// <returns>The initial corresponding to the specified <paramref name="dir"/>.</returns>
         [Pure] public static string ToLetters(this Dir24 dir)
             => directions24[(int)dir];
 
+        /// <summary>
+        ///   <para>Rotates the specified <paramref name="dir"/> by the specified amount of 90° steps clockwise.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to rotate.</param>
+        /// <param name="delta">The amount of 90° steps. Negative value rotates counter-clockwise.</param>
+        /// <returns>The specified <paramref name="dir"/> rotated clockwise by the specified amount of steps.</returns>
         [Pure] public static Dir4 Rotate(this Dir4 dir, int delta)
             => Direction.Normalize(RotateCore(dir, delta));
+        /// <summary>
+        ///   <para>Rotates the specified <paramref name="dir"/> by the specified amount of 45° steps clockwise.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to rotate.</param>
+        /// <param name="delta">The amount of 45° steps. Negative value rotates counter-clockwise.</param>
+        /// <returns>The specified <paramref name="dir"/> rotated clockwise by the specified amount of steps.</returns>
         [Pure] public static Dir8 Rotate(this Dir8 dir, int delta)
             => Direction.Normalize(RotateCore(dir, delta));
+        /// <summary>
+        ///   <para>Rotates the specified <paramref name="dir"/> by the specified amount of 15° steps clockwise.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to rotate.</param>
+        /// <param name="delta">The amount of 15° steps. Negative value rotates counter-clockwise.</param>
+        /// <returns>The specified <paramref name="dir"/> rotated clockwise by the specified amount of steps.</returns>
         [Pure] public static Dir24 Rotate(this Dir24 dir, int delta)
             => Direction.Normalize(RotateCore(dir, delta));
 
+        /// <summary>
+        ///   <para>Rotates the specified <paramref name="dir"/> one step (90°) clockwise.</para>
+        /// </summary>
+        /// <param name="dir">The direction to rotate 90° clockwise.</param>
+        /// <returns>The specified <paramref name="dir"/> rotated 90° clockwise.</returns>
         [Pure] public static Dir4 Clockwise(this Dir4 dir)
             => Direction.Normalize(RotateCore(dir, 1));
+        /// <summary>
+        ///   <para>Rotates the specified <paramref name="dir"/> one step (45°) clockwise.</para>
+        /// </summary>
+        /// <param name="dir">The direction to rotate 45° clockwise.</param>
+        /// <returns>The specified <paramref name="dir"/> rotated 45° clockwise.</returns>
         [Pure] public static Dir8 Clockwise(this Dir8 dir)
             => Direction.Normalize(RotateCore(dir, 1));
+        /// <summary>
+        ///   <para>Rotates the specified <paramref name="dir"/> one step (15°) clockwise.</para>
+        /// </summary>
+        /// <param name="dir">The direction to rotate 15° clockwise.</param>
+        /// <returns>The specified <paramref name="dir"/> rotated 15° clockwise.</returns>
         [Pure] public static Dir24 Clockwise(this Dir24 dir)
             => Direction.NormalizeFast(RotateCore(dir, 1));
 
@@ -60,18 +141,48 @@ namespace SoRR
 
         // the conversion methods can't be generalized properly without losing performance
 
+        /// <summary>
+        ///   <para>Converts the specified <see cref="Dir4"/> <paramref name="dir"/> to a corresponding <see cref="Dir8"/> direction.</para>
+        /// </summary>
+        /// <param name="dir">The <see cref="Dir4"/> direction to convert to <see cref="Dir8"/>.</param>
+        /// <returns>The <see cref="Dir8"/> direction corresponding to the specified <paramref name="dir"/>.</returns>
         [Pure] public static Dir8 ToDir8(this Dir4 dir)
             => (Dir8)((int)dir * 2);
+        /// <summary>
+        ///   <para>Converts the specified <see cref="Dir4"/> <paramref name="dir"/> to a corresponding <see cref="Dir24"/> direction.</para>
+        /// </summary>
+        /// <param name="dir">The <see cref="Dir4"/> direction to convert to <see cref="Dir24"/>.</param>
+        /// <returns>The <see cref="Dir24"/> direction corresponding to the specified <paramref name="dir"/>.</returns>
         [Pure] public static Dir24 ToDir24(this Dir4 dir)
             => (Dir24)((int)dir * 6);
 
+        /// <summary>
+        ///   <para>Converts the specified <see cref="Dir8"/> <paramref name="dir"/> to the closest <see cref="Dir4"/> direction.</para>
+        /// </summary>
+        /// <param name="dir">The <see cref="Dir8"/> direction to convert to <see cref="Dir4"/>.</param>
+        /// <returns>The <see cref="Dir4"/> direction closest to the specified <paramref name="dir"/>, rounding down/counter-clockwise.</returns>
         [Pure] public static Dir4 ToDir4(this Dir8 dir)
             => (Dir4)((int)dir / 2);
+        /// <summary>
+        ///   <para>Converts the specified <see cref="Dir8"/> <paramref name="dir"/> to a corresponding <see cref="Dir24"/> direction.</para>
+        /// </summary>
+        /// <param name="dir">The <see cref="Dir8"/> direction to convert to <see cref="Dir24"/>.</param>
+        /// <returns>The <see cref="Dir24"/> direction corresponding to the specified <paramref name="dir"/>.</returns>
         [Pure] public static Dir24 ToDir24(this Dir8 dir)
             => (Dir24)((int)dir * 3);
 
+        /// <summary>
+        ///   <para>Converts the specified <see cref="Dir24"/> <paramref name="dir"/> to the closest <see cref="Dir4"/> direction.</para>
+        /// </summary>
+        /// <param name="dir">The <see cref="Dir24"/> direction to convert to <see cref="Dir4"/>.</param>
+        /// <returns>The <see cref="Dir4"/> direction closest to the specified <paramref name="dir"/>, rounding down/counter-clockwise.</returns>
         [Pure] public static Dir4 ToDir4(this Dir24 dir)
             => (Dir4)((((int)dir + 2) / 6) & 3);
+        /// <summary>
+        ///   <para>Converts the specified <see cref="Dir24"/> <paramref name="dir"/> to the closest <see cref="Dir8"/> direction.</para>
+        /// </summary>
+        /// <param name="dir">The <see cref="Dir24"/> direction to convert to <see cref="Dir8"/>.</param>
+        /// <returns>The <see cref="Dir4"/> direction closest to the specified <paramref name="dir"/>, rounding down/counter-clockwise.</returns>
         [Pure] public static Dir8 ToDir8(this Dir24 dir)
             => (Dir8)((((int)dir + 1) / 3) & 7);
 
--- SoRR/Directions.cs
+++ SoRR/Directions.cs
@@ -5,30 +5,69 @@ using UnityEngine;
 
 namespace SoRR
 {
+    /// <summary>
+    ///   <para>Provides information about, and means to create direction enumerations.</para>
+    /// </summary>
     public static class Direction
     {
+        /// <summary>
+        ///   <para>Returns the amount of directions defined in the <typeparamref name="T"/> direction enum.</para>
+        /// </summary>
+        /// <typeparam name="T">The direction enumeration type.</typeparam>
+        /// <returns>The amount of directions defined in the <typeparamref name="T"/> direction enum.</returns>
+        /// <exception cref="NotSupportedException">Type <typeparamref name="T"/> is not a direction enum.</exception>
         [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int CountOf<T>()
         {
             if (typeof(T) == typeof(Dir4)) return 4;
             if (typeof(T) == typeof(Dir8)) return 8;
             if (typeof(T) == typeof(Dir24)) return 24;
-            throw new ArgumentException($"Type {typeof(T)} is not a direction enum.", nameof(T));
+            throw new NotSupportedException($"Type {typeof(T)} is not a direction enum.");
         }
 
+        /// <summary>
+        ///   <para>Converts the specified rotation, in <paramref name="degrees"/>, to a <see cref="Dir4"/> direction.</para>
+        /// </summary>
+        /// <param name="degrees">The rotation angle, in degrees clockwise.</param>
+        /// <returns>A <see cref="Dir4"/> direction closest to the specified rotation.</returns>
         [Pure] public static Dir4 DegreesToDir4(float degrees)
             => Normalize(UnitsToDirCore<Dir4>(degrees, 360f));
+        /// <summary>
+        ///   <para>Converts the specified rotation, in <paramref name="degrees"/>, to a <see cref="Dir8"/> direction.</para>
+        /// </summary>
+        /// <param name="degrees">The rotation angle, in degrees clockwise.</param>
+        /// <returns>A <see cref="Dir8"/> direction closest to the specified rotation.</returns>
         [Pure] public static Dir8 DegreesToDir8(float degrees)
             => Normalize(UnitsToDirCore<Dir8>(degrees, 360f));
+        /// <summary>
+        ///   <para>Converts the specified rotation, in <paramref name="degrees"/>, to a <see cref="Dir24"/> direction.</para>
+        /// </summary>
+        /// <param name="degrees">The rotation angle, in degrees clockwise.</param>
+        /// <returns>A <see cref="Dir24"/> direction closest to the specified rotation.</returns>
         [Pure] public static Dir24 DegreesToDir24(float degrees)
-            => NormalizeFast(UnitsToDirCore<Dir24>(degrees, 360f));
+            => Normalize(UnitsToDirCore<Dir24>(degrees, 360f));
 
+        /// <summary>
+        ///   <para>Converts the specified rotation, in <paramref name="radians"/>, to a <see cref="Dir4"/> direction.</para>
+        /// </summary>
+        /// <param name="radians">The rotation angle, in radians clockwise.</param>
+        /// <returns>A <see cref="Dir4"/> direction closest to the specified rotation.</returns>
         [Pure] public static Dir4 RadiansToDir4(float radians)
             => Normalize(UnitsToDirCore<Dir4>(radians, 2f * Mathf.PI));
+        /// <summary>
+        ///   <para>Converts the specified rotation, in <paramref name="radians"/>, to a <see cref="Dir8"/> direction.</para>
+        /// </summary>
+        /// <param name="radians">The rotation angle, in radians clockwise.</param>
+        /// <returns>A <see cref="Dir8"/> direction closest to the specified rotation.</returns>
         [Pure] public static Dir8 RadiansToDir8(float radians)
             => Normalize(UnitsToDirCore<Dir8>(radians, 2f * Mathf.PI));
+        /// <summary>
+        ///   <para>Converts the specified rotation, in <paramref name="radians"/>, to a <see cref="Dir24"/> direction.</para>
+        /// </summary>
+        /// <param name="radians">The rotation angle, in radians clockwise.</param>
+        /// <returns>A <see cref="Dir24"/> direction closest to the specified rotation.</returns>
         [Pure] public static Dir24 RadiansToDir24(float radians)
-            => NormalizeFast(UnitsToDirCore<Dir24>(radians, 2f * Mathf.PI));
+            => Normalize(UnitsToDirCore<Dir24>(radians, 2f * Mathf.PI));
 
         [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static TDir UnitsToDirCore<TDir>(float units, float fullRotation)
@@ -37,30 +76,71 @@ namespace SoRR
             return Unsafe.As<int, TDir>(ref value);
         }
 
+        /// <summary>
+        ///   <para>Determines whether the specified <paramref name="dir"/> specifies a valid <see cref="Dir4"/> direction.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to validate.</param>
+        /// <returns><see langword="true"/>, if <paramref name="dir"/> specifies a valid <see cref="Dir4"/> direction; otherwise, <see langword="false"/>.</returns>
         [Pure] public static bool IsValid(Dir4 dir)
             => IsValidCore(dir);
+        /// <summary>
+        ///   <para>Determines whether the specified <paramref name="dir"/> specifies a valid <see cref="Dir8"/> direction.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to validate.</param>
+        /// <returns><see langword="true"/>, if <paramref name="dir"/> specifies a valid <see cref="Dir8"/> direction; otherwise, <see langword="false"/>.</returns>
         [Pure] public static bool IsValid(Dir8 dir)
             => IsValidCore(dir);
+        /// <summary>
+        ///   <para>Determines whether the specified <paramref name="dir"/> specifies a valid <see cref="Dir24"/> direction.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to validate.</param>
+        /// <returns><see langword="true"/>, if <paramref name="dir"/> specifies a valid <see cref="Dir24"/> direction; otherwise, <see langword="false"/>.</returns>
         [Pure] public static bool IsValid(Dir24 dir)
             => IsValidCore(dir);
 
         [Pure] private static bool IsValidCore<TDir>(TDir dir)
             => Unsafe.As<TDir, uint>(ref dir) < CountOf<TDir>();
 
+        /// <summary>
+        ///   <para>Normalizes the specified <paramref name="dir"/> to the range of valid <see cref="Dir4"/> directions.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to normalize.</param>
+        /// <returns>The specified <paramref name="dir"/>, normalized to the range of valid <see cref="Dir4"/> directions.</returns>
         [Pure] public static Dir4 Normalize(Dir4 dir)
             => (Dir4)((int)dir & 3);
+        /// <summary>
+        ///   <para>Normalizes the specified <paramref name="dir"/> to the range of valid <see cref="Dir8"/> directions.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to normalize.</param>
+        /// <returns>The specified <paramref name="dir"/>, normalized to the range of valid <see cref="Dir8"/> directions.</returns>
         [Pure] public static Dir8 Normalize(Dir8 dir)
             => (Dir8)((int)dir & 7);
+        /// <summary>
+        ///   <para>Normalizes the specified <paramref name="dir"/> to the range of valid <see cref="Dir24"/> directions.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to normalize.</param>
+        /// <returns>The specified <paramref name="dir"/>, normalized to the range of valid <see cref="Dir24"/> directions.</returns>
         [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Dir24 Normalize(Dir24 dir)
         {
             int result = (int)dir % 24;
             return (Dir24)(result < 0 ? result + 24 : result);
         }
+        /// <summary>
+        ///   <para>Normalizes the specified <paramref name="dir"/> to the range of valid <see cref="Dir24"/> directions, using a fast-path that only normalizes <c>(Dir24)24</c> to <c>0</c>.</para>
+        /// </summary>
+        /// <param name="dir">The direction value to normalize.</param>
+        /// <returns>The specified <paramref name="dir"/>, normalized to the range of valid <see cref="Dir24"/> directions.</returns>
         [Pure] public static Dir24 NormalizeFast(Dir24 dir)
-            => dir == (Dir24)24 ? 0 : dir;
+        {
+            Debug.Assert((int)dir is >= 0 and <= 24);
+            return dir == (Dir24)24 ? 0 : dir;
+        }
 
     }
+    /// <summary>
+    ///   <para>Defines the four cardinal directions: <see cref="North"/>, <see cref="East"/>, <see cref="South"/> and <see cref="West"/>.</para>
+    /// </summary>
     public enum Dir4
     {
         North,
@@ -68,6 +148,9 @@ namespace SoRR
         South,
         West,
     }
+    /// <summary>
+    ///   <para>Defines the four cardinal and four intercardinal directions: N, NE, E, SE, S, SW, W, NW.</para>
+    /// </summary>
     public enum Dir8
     {
         North,
@@ -79,6 +162,9 @@ namespace SoRR
         West,
         NorthWest,
     }
+    /// <summary>
+    ///   <para>Defines 24 directions: four cardinal, four intercardinal and 16 in-between them (2 per every division).</para>
+    /// </summary>
     public enum Dir24
     {
         // ReSharper disable InconsistentNaming

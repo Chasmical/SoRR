79d1cadcb5ce8e6ce7cc6bdd0ae1a1c9ad6697fe
Chasmical <hillankeyxl2@yandex.ru>
2024-10-15 14:48:03 +0700

    📝 Add some XML docs

--- SoRR/Assets/FileUtility.cs
+++ SoRR/Assets/FileUtility.cs
@@ -6,10 +6,33 @@ using Newtonsoft.Json;
 
 namespace SoRR
 {
+    /// <summary>
+    ///   <para>Provides a set of static methods for working with files.</para>
+    /// </summary>
     public static class FileUtility
     {
+        /// <summary>
+        ///   <para>Opens the specified file and deserializes its contents as a JSON of the specified <typeparamref name="T"/> type.</para>
+        /// </summary>
+        /// <typeparam name="T">The type of the serialized data.</typeparam>
+        /// <param name="filePath">A path to the file to open.</param>
+        /// <returns>The deserialized contents of the specified file.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="filePath"/> is <see langword="null"/>.</exception>
+        /// TODO: path, file and json exceptions
         [MustUseReturnValue] public static T? ReadJson<T>(string filePath)
-            => ReadJson<T>(File.OpenRead(filePath));
+        {
+            if (filePath is null) throw new ArgumentNullException(nameof(filePath));
+            return ReadJson<T>(File.OpenRead(filePath));
+        }
+        /// <summary>
+        ///   <para>Deserializes the contents of the specified <paramref name="stream"/> as a JSON of the specified <typeparamref name="T"/> type.</para>
+        /// </summary>
+        /// <typeparam name="T">The type of the serialized data.</typeparam>
+        /// <param name="stream">A stream containing the serialized data.</param>
+        /// <returns>The deserialized contents of the specified <paramref name="stream"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="stream"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="stream"/> does not support reading.</exception>
+        /// TODO: json exceptions
         [MustUseReturnValue] public static T? ReadJson<T>([HandlesResourceDisposal] Stream stream)
         {
             if (stream is null) throw new ArgumentNullException(nameof(stream));
@@ -20,8 +43,28 @@ namespace SoRR
                 return new JsonSerializer().Deserialize<T>(jsonReader);
         }
 
+        /// <summary>
+        ///   <para>Creates or overwrites the specified file and serializes the specified <paramref name="value"/> as JSON to it.</para>
+        /// </summary>
+        /// <typeparam name="T">The type of the data to serialize.</typeparam>
+        /// <param name="filePath">A path to the file to create or overwrite.</param>
+        /// <param name="value">The value to serialize as JSON.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="filePath"/> is <see langword="null"/>.</exception>
+        /// TODO: path, file and json exceptions
         public static void WriteJson<T>(string filePath, T? value)
-            => WriteJson(File.Create(filePath), value);
+        {
+            if (filePath is null) throw new ArgumentNullException(nameof(filePath));
+            WriteJson(File.Create(filePath), value);
+        }
+        /// <summary>
+        ///   <para>Serializes the specified <paramref name="value"/> as JSON to the specified <paramref name="stream"/>.</para>
+        /// </summary>
+        /// <typeparam name="T">The type of the data to serialize.</typeparam>
+        /// <param name="stream">A stream to serialize the specified <paramref name="value"/> as JSON to.</param>
+        /// <param name="value">The value to serialize as JSON.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="stream"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="stream"/> is not writable.</exception>
+        /// TODO: json exceptions
         public static void WriteJson<T>([HandlesResourceDisposal] Stream stream, T? value)
         {
             if (stream is null) throw new ArgumentNullException(nameof(stream));
@@ -32,8 +75,17 @@ namespace SoRR
                 new JsonSerializer().Serialize(jsonWriter, value, typeof(T));
         }
 
+        /// <summary>
+        ///   <para>Enumerates through all files in the specified <paramref name="directoryPath"/> matching the specified <paramref name="pathWithoutExtension"/>.</para>
+        /// </summary>
+        /// <param name="directoryPath">A path to the directory to search in.</param>
+        /// <param name="pathWithoutExtension">A file sub-path, without the extension, relative to the specified directory.</param>
+        /// <returns>An enumerable collection of the full paths for the files in the specified directory matching the specified <paramref name="pathWithoutExtension"/>.</returns>
+        /// TODO: path exceptions
         [Pure] public static IEnumerable<string> SearchFiles(string directoryPath, string pathWithoutExtension)
         {
+            if (directoryPath is null) throw new ArgumentNullException(nameof(directoryPath));
+            if (pathWithoutExtension is null) throw new ArgumentNullException(nameof(pathWithoutExtension));
             if (!Directory.Exists(directoryPath)) yield break;
 
             string nameWithoutExtension = Path.GetFileName(pathWithoutExtension);
--- SoRR/Components/MainThread.cs
+++ SoRR/Components/MainThread.cs
@@ -8,25 +8,63 @@ using UnityEngine;
 
 namespace SoRR
 {
+    /// <summary>
+    ///   <para>Provides a set of static methods for running code on the main Unity thread.</para>
+    /// </summary>
     public static class MainThread
     {
         private static readonly ConcurrentQueue<Action> queue = [];
         private static MainThreadRunner? activeRunner;
 
+        /// <summary>
+        ///   <para>Determines whether the current thread is the main Unity thread.</para>
+        /// </summary>
+        /// <returns><see langword="true"/>, if the current thread is the main Unity thread; otherwise, <see langword="false"/>.</returns>
+        [Pure] public static bool IsCurrent()
+            => activeRunner is { } runner && runner.ThreadId == Thread.CurrentThread.ManagedThreadId;
+
+        /// <summary>
+        ///   <para>Enqueues the specified <paramref name="action"/> to run on the main Unity thread.</para>
+        /// </summary>
+        /// <param name="action">The action to run on the main Unity thread.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="action"/> is <see langword="null"/>.</exception>
         public static void Enqueue(Action action)
         {
-            // TODO: log a warning, if activeRunner is null
+            if (action is null) throw new ArgumentNullException(nameof(action));
+            // TODO: log a warning if activeRunner is null or disabled
             queue.Enqueue(action);
         }
 
-        [UsedImplicitly]
+
+        /// <summary>
+        ///   <para>Represents a component responsible for querying and executing the queue of tasks scheduled to run on the main Unity thread.</para>
+        /// </summary>
         public sealed class MainThreadRunner : MonoBehaviour
         {
+            /// <summary>
+            ///   <para>Gets the unique identifier of the thread that the runner was initialized on.</para>
+            /// </summary>
+            public int ThreadId { get; private set; }
+
+            // Store lists to avoid allocating them every time
             private readonly List<Action> actionsList = [];
             private readonly List<Exception> exceptionsList = [];
 
+            /// <summary/>
+            public void Awake()
+                => ThreadId = Thread.CurrentThread.ManagedThreadId;
+            /// <summary/>
+            public void OnEnable()
+                => enabled = Interlocked.CompareExchange(ref activeRunner, this, null) is null;
+            /// <summary/>
+            public void OnDisable()
+                => Interlocked.CompareExchange(ref activeRunner, null, this);
+
+            /// <summary/>
             public void FixedUpdate()
             {
+                Debug.Assert(IsCurrent());
+
                 List<Action> actions = actionsList;
                 List<Exception> exceptions = exceptionsList;
 
@@ -47,12 +85,6 @@ namespace SoRR
                     throw exception;
                 }
             }
-
-            // There can only be one
-            public void OnEnable()
-                => enabled = Interlocked.CompareExchange(ref activeRunner, this, null) is null;
-            public void OnDisable()
-                => Interlocked.CompareExchange(ref activeRunner, null, this);
 
         }
     }

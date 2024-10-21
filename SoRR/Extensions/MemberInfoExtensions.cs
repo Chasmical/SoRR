431933da7067f8c9702a7bd38d53b8999a14a155
Chasmical <hillankeyxl2@yandex.ru>
2024-10-21 23:32:27 +0700

    ✨ Add nullability reflection extensions

--- SoRR/Extensions/MemberInfoExtensions.cs
+++ SoRR/Extensions/MemberInfoExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using JetBrains.Annotations;
@@ -62,5 +64,112 @@ namespace SoRR
             return false;
         }
 
+        /// <summary>
+        ///   <para>Returns the nullability of the specified <paramref name="field"/>.</para>
+        /// </summary>
+        /// <param name="field">The field to determine the nullability of.</param>
+        /// <returns>The nullability of the specified <paramref name="field"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="field"/> is <see langword="null"/>.</exception>
+        [Pure] public static Nullability GetNullability(this FieldInfo field)
+        {
+            if (field is null) throw new ArgumentNullException(nameof(field));
+            return GetNullabilityCore(field.FieldType, field.DeclaringType, field.GetCustomAttributesData());
+        }
+        /// <summary>
+        ///   <para>Returns the nullability of the specified <paramref name="property"/>.</para>
+        /// </summary>
+        /// <param name="property">The property to determine the nullability of.</param>
+        /// <returns>The nullability of the specified <paramref name="property"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="property"/> is <see langword="null"/>.</exception>
+        [Pure] public static Nullability GetNullability(this PropertyInfo property)
+        {
+            if (property is null) throw new ArgumentNullException(nameof(property));
+            return GetNullabilityCore(property.PropertyType, property.DeclaringType, property.GetCustomAttributesData());
+        }
+        /// <summary>
+        ///   <para>Returns the nullability of the specified <paramref name="parameter"/>.</para>
+        /// </summary>
+        /// <param name="parameter">The parameter to determine the nullability of.</param>
+        /// <returns>The nullability of the specified <paramref name="parameter"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="parameter"/> is <see langword="null"/>.</exception>
+        [Pure] public static Nullability GetNullability(this ParameterInfo parameter)
+        {
+            if (parameter is null) throw new ArgumentNullException(nameof(parameter));
+            return GetNullabilityCore(parameter.ParameterType, parameter.Member, parameter.GetCustomAttributesData());
+        }
+
+        [Pure] private static Nullability GetNullabilityCore(Type type, MemberInfo? parent, IList<CustomAttributeData> attributes)
+        {
+            if (type.IsByRef || type.IsPointer)
+                type = type.GetElementType()!;
+
+            if (type.IsValueType)
+                return Nullable.GetUnderlyingType(type) is not null ? Nullability.Nullable : Nullability.NotNull;
+
+            object? value = GetNullabilityFirstAttributeValue(attributes, "NullableAttribute");
+            Nullability result = ParseNullableState(value);
+
+            if (result == Nullability.Unknown)
+            {
+                while (parent is not null)
+                {
+                    value = GetNullabilityFirstAttributeValue(parent.GetCustomAttributesData(), "NullableContextAttribute");
+                    if (value is byte b)
+                    {
+                        result = (Nullability)b;
+                        break;
+                    }
+                    parent = parent.DeclaringType;
+                }
+            }
+            return result;
+        }
+        [Pure] private static object? GetNullabilityFirstAttributeValue(IList<CustomAttributeData> attributes, string attrName)
+        {
+            const string attrNamespace = "System.Runtime.CompilerServices";
+
+            foreach (CustomAttributeData attr in attributes)
+            {
+                Type type = attr.AttributeType;
+                if (type.Name == attrName && type.Namespace == attrNamespace)
+                {
+                    IList<CustomAttributeTypedArgument> ctorArgs = attr.ConstructorArguments;
+                    if (ctorArgs.Count == 1) return ctorArgs[0].Value;
+                }
+            }
+            return null;
+        }
+        [Pure] private static Nullability ParseNullableState(object? state)
+        {
+            switch (state)
+            {
+                case byte b:
+                    return (Nullability)b;
+                case ReadOnlyCollection<CustomAttributeTypedArgument> args
+                    when args.Count > 0 && args[0].Value is byte b:
+                    return (Nullability)b;
+                default:
+                    return Nullability.Unknown;
+            }
+        }
+
+    }
+    /// <summary>
+    ///   <para>Defines nullability modes of fields, properties and parameters.</para>
+    /// </summary>
+    public enum Nullability
+    {
+        /// <summary>
+        ///   <para>Specifies that the value's nullability is unknown.</para>
+        /// </summary>
+        Unknown = 0,
+        /// <summary>
+        ///   <para>Specifies that the value is not null.</para>
+        /// </summary>
+        NotNull = 1,
+        /// <summary>
+        ///   <para>Specifies that the value may be null.</para>
+        /// </summary>
+        Nullable = 2,
     }
 }
